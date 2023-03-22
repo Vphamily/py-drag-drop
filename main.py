37cd54183bc1a0fb68a279eba46dde63166c778d
Van Pham <vpham@unitedbenefits.local>
2023-03-22 10:34:41 -0500

    add imports route

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import pandas as pd
 app = Flask(__name__)
 
 allowed_extensions = {'.csv'}
-app.secret_key = 'mysecretkey'
+app.secret_key = 'loveithere'
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+# upload route
 @app.route('/upload', methods=['POST', 'GET'])
 def upload():
     if request.method == 'POST':
@@ -22,11 +23,17 @@ def upload():
             session['data'] = data 
     return render_template('detail.html', data = session['data'])
     
-
 # redirect route
 @app.route('/back')
 def back():
     return redirect(url_for('index'))
+
+# redirect route sending the data from df to mysql database 
+# modify if need to use other db
+@app.route('/imports', methods=['POST', 'GET'])
+def imports():
+
+    return "import scucessfully"
 
 if __name__ == '__main__':
     app.run()
